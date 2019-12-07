bd28fedbed19007a205d1b0ff2aef4bd863839d4
David Baddeley <david.baddeley.nz@gmail.com>
2019-12-07 21:24:44 +0000

    changes to recipes to make visualc happy

--- pyfftw/bld.bat
+++ pyfftw/bld.bat
@@ -1,3 +1,7 @@
 xcopy %PREFIX%\DLLs\libfftw3* %SRC_DIR%\pyfftw\ /f
 
+rem Cludge to make us find the community edition of visualc
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+
+"%PYTHON%" setup.py build
 "%PYTHON%" setup.py install
