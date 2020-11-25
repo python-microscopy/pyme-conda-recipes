08da39d4e5d60185ec5a681f48cc1c0012e2a33c
David Baddeley <david.baddeley.nz@gmail.com>
2020-11-26 12:01:30 +1300

    make sure fftw libs are on path for OSX build

--- pyfftw/build.sh
+++ pyfftw/build.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+if [ -n "$OSX_ARCH" ]
+    then
+        export DYLD_LIBRARY_PATH="$PREFIX/lib"
+        export LD_FLAGS="-L$PREFIX/lib"
+        export CFLAGS="-I$PREFIX/include"
+
+fi
+
 $PYTHON setup.py install
