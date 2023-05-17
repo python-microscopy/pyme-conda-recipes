8bdfc46be3d7e194f589e43ae1c9a1747c65fd12
David Baddeley <david.baddeley.nz@gmail.com>
2023-05-17 20:24:45 +1200

    add recipe for ch_shrinkwrap

---
+++ ch_shrinkwrap/bld.bat
@@ -0,0 +1,22 @@
+:: "%PYTHON%" setup.py build_ext -c mingw32
+
+rem Cludge to make us find the community edition of visualc
+echo %MSVC_PATH%
+if defined MSVC_PATH (
+call "%MSVC_PATH%\vcvarsall.bat" x64
+) else call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+
+"%PYTHON%" setup.py install
+
+:: copy "%RECIPE_DIR%"\libpython27.a "%PREFIX%"\libs
+
+:: "%PYTHON%" pymecompress\setup.py build --compiler=mingw32
+:: "%PYTHON%" pymecompress\setup.py install 
+
+if errorlevel 1 exit 1
+
+:: Add more build steps here, if they are necessary.
+
+:: See
+:: http://docs.continuum.io/conda/build.html
+:: for a list of environment variables that are set during the build process.
---
+++ ch_shrinkwrap/build.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+cd "PYME"
+
+if [ -n "$OSX_ARCH" ]
+    then
+        $PYTHON setup.py build
+        $PREFIX/python.app/Contents/MacOS/python setup.py install
+else
+    $PYTHON setup.py install
+fi
+
+$PYTHON install_plugin.py
+#$PYTHON pymecompress/setup.py install
+
+# Add more build steps here, if they are necessary.
+
+
+
+#echo "Attempting to build and install go components"
+#
+#curdir=`pwd`
+#
+#PYMEGOdir="$GOPATH/src/github.com/mrd0ll4r/pyme"
+#if [ -d "$PYMEGOdir" ]
+#    then
+#        echo "installing pyme go components"
+#
+#        cd $PYMEGOdir/cmd/distributor
+#        go install .
+#
+#        cd $PYMEGOdir/cmd/nodeserver
+#        go install .
+#
+#        cd $curdir
+#
+#        cp $GOBIN/distributor $PREFIX/bin
+#        cp $GOBIN/nodeserver $PREFIX/bin
+#fi
+
+
+# See
+# http://docs.continuum.io/conda/build.html
+# for a list of environment variables that are set during the build process.
